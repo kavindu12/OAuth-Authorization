39a2ca99a668796d2102badcadd104a9e421e92a
kavindu12 <kavindu.amarawansha@gmail.com>
2019-09-08 09:26:41 +0530

    Added new files

---
+++ oauth-springboot/src/main/java/springboot/oauth/oauthspringboot/OauthSpringbootApplication.java
@@ -0,0 +1,13 @@
+package springboot.oauth.oauthspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSpringbootApplication.class, args);
+	}
+
+}
